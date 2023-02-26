3698fb57af119f1de54bc8cc6d2cd478c8f10f4a
Samarjit Debnath <samarjit_debnath@Samarjits-MacBook-Air.local>
2023-02-26 22:30:10 +0530

    Read for deployment

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ def predict():
     return render_template("index.html", prediction_text="The Price for this house is {0}".format(output))
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(host='0.0.0.0', port=8080,  debug=False)
